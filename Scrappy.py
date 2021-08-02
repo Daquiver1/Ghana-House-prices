552e7761b979de2ec46390266161cbdc1cbb61d5
Christian Abrokwa <cabrokwa11@gmail.com>
2021-08-02 16:59:56 +0000

    Added comments

--- Scrappy.py
+++ Scrappy.py
@@ -1,15 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Storing the webpage
 url = "https://meqasa.com/houses-for-sale-in-East%20Legon"
 page = requests.get(url)
 
+# Locating where the prices,bedroom,baths and garages are stored
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="pg1")
 mqs_class = results.find_all("div", class_= "mqs-prop-dt-wrapper")
 # In results, find all p and ul tags with attribues h3 and prop-features
 test = results.find_all(['p', 'ul'], ['h3', 'prop-features'])
 
+# Storing the data into a text file named house_details
 houses_file = open("houses_details.txt", "w")
 
 for data in mqs_class: # Open the mqs-prop-dt-wrapper tag class
@@ -18,6 +21,7 @@ for data in mqs_class: # Open the mqs-prop-dt-wrapper tag class
         #count += 1, there are 534 instances, that's more than enough to train our model
         houses_file.writelines(trial)
 
+# Removing the whitespaces
 lines = []
 
 with open("/content/houses_details.txt") as f:
@@ -25,6 +29,8 @@ with open("/content/houses_details.txt") as f:
         line = line.strip()
         if line:
             lines.append(line)
+# Storing the data in a txt file but this time each
+# entry is stored on a single line
 
 houses_file2 = open("houses_details1.txt", "w")
 
