8b1cfbd39abf04b8e699d40266e04868c1815362
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-05-03 08:03:35 +0000

    Stored data in text file

--- main.py
+++ main.py
@@ -1,42 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Storing the webpage
-url = "https://meqasa.com/houses-for-sale-in-East%20Legon"
+# Get url
+url = "https://meqasa.com/houses-for-sale-in-Accra.html?y=965793785&w=1"
 page = requests.get(url)
+soup = BeautifulSoup(page.content, "lxml")
 
-# Locating where the prices,bedroom,baths and garages are stored
-soup = BeautifulSoup(page.content, "html.parser")
+# Locate where it's stored
 results = soup.find(id="pg1")
-mqs_class = results.find_all("div", class_= "mqs-prop-dt-wrapper")
-# In results, find all p and ul tags with attribues h3 and prop-features
-test = results.find_all(['p', 'ul'], ['h3', 'prop-features'])
+mqs_class = results.find_all(
+    "div", class_="row mqs-featured-prop-inner-wrap clickable "
+)
 
-# Storing the data into a text file named house_details
-houses_file = open("houses_details.txt", "w")
+data_file = open("data/temp.txt", "w")
 
-for data in mqs_class: # Open the mqs-prop-dt-wrapper tag class
-    for instances in test:
-        trial = instances.get_text()
-        #count += 1, there are 534 instances, that's more than enough to train our model
-        houses_file.writelines(trial)
-
-# Removing the whitespaces
-lines = []
-
-with open("/content/houses_details.txt") as f:
-    for line in f:
-        line = line.strip()
-        if line:
-            lines.append(line)
-# Storing the data in a txt file but this time each
-# entry is stored on a single line
-
-houses_file2 = open("houses_details1.txt", "w")
-
-for element in lines[:]:
-    if element.startswith("P"):
-        houses_file2.write("\n") #adds a newline
-    houses_file2.write(element)
-    houses_file2.write(", ")
-
+for i in mqs_class:
+    description = i.find(["h2"]).get_text().strip()  # get description
+    price = i.find(["p"], ["h3"]).get_text().strip()  # get the prices
+    room_num = (
+        i.find(["li"], ["bed"]).get_text().strip()
+    )  # get the number of rooms
+    # print(f"{description} {price} {room_num}")
+    data_file.write(
+        f"{description}, {price}, {room_num}"
+    )  # store it in the text file
+    data_file.write("\n")
