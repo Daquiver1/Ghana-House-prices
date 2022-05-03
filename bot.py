45c0fe9edd9121ebb4bb5be1d317dd5410cfc05c
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-05-03 15:19:09 +0000

    documented code

--- bot.py
+++ bot.py
@@ -4,29 +4,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from crawler import get_details
 import time
-
-
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+# setup settings
 s = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
 options.headless = true
 driver = webdriver.Chrome(service=s, options=options)
 
 URL = "https://meqasa.com/houses-for-sale-in-Accra.html?y=965793785&w=1"
-
+NUM = 200 # number of pages you want to crawl
 
 driver.get(URL)
 next_button = driver.find_element(By.ID, "pagenumnext")
 
-for i in range(2):
+for i in range(NUM):
+    # click on the next button and wait for the pg to load
+    # it's a js button, so have to use execute_script
     driver.execute_script("arguments[0].click();", next_button)
     value = f"pg{2+i}"
     print(value)
     WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, value)))
 
 
+# retrieve the current page source
 source1 = driver.execute_script("return document.body.innerHTML;")
 get_details(source1)
 
--- crawler.py
+++ crawler.py
@@ -1,25 +1,34 @@
-import requests
+import re
 from bs4 import BeautifulSoup
 
 
 def get_details(source):
- 
+    """
+    A function to get the description, prices and room number of houses"
+
+    arguments: The page source. Type = String
+    output: A text file storing the description, prices and room num separated
+    by a comma.
+
+    """
+
     soup = BeautifulSoup(source, "lxml")
     results = soup.find(id="listview")
-    mqs_class = results.find_all("div", class_=re.compile("row mqs-featured-prop-inner-wrap clickable "))
+    mqs_class = results.find_all(
+        "div", class_=re.compile("row mqs-featured-prop-inner-wrap clickable ")
+    )
 
-
-    data_file = open("/content/temp.txt", "w")
+    data_file = open("data/temp.txt", "w")
     for i in mqs_class:
+        # iterate through the values and store in text file
         description = i.find(["h2"]).get_text().strip()
         price = i.find(["p"], ["h3"]).get_text().strip()
-        room_num = i.find( ["li"], ["bed"]).get_text().strip()
-        print(f"{description} {price} {room_num}")    
+        room_num = i.find(["li"], ["bed"]).get_text().strip()
         data_file.write(f"{description}, {price}, {room_num}")
         data_file.write("\n")
 
     data_file.close()
- 
+
 
 if __name__ == "__main__":
     pass
