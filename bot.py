34dbe0db13d76ae1572d8163e9ef04b0b0e92a88
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-05-03 11:05:53 +0000

    Finished extracting data with selenium

--- bot.py
+++ bot.py
@@ -1,22 +1,17 @@
 from selenium import webdriver
-import os
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
 from crawler import get_details
-import requests
 import time
 
-# Used when polling.
 
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# PATH = "path to folder"
 s = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
-# options.headless = false
+# options.headless = true
 driver = webdriver.Chrome(service=s, options=options)
 
 URL = "https://meqasa.com/houses-for-sale-in-Accra.html?y=965793785&w=1"
@@ -24,39 +19,11 @@ URL = "https://meqasa.com/houses-for-sale-in-Accra.html?y=965793785&w=1"
 # Log in
 driver.get(URL)
 driver.find_element(By.ID, "redi-modal-close").click()
+print(driver.current_url)
 get_details(driver.current_url)
 
-next_button = driver.find_element(By.ID, "pagenumnext")
-# It's a js button, so need to use execute script
-driver.execute_script("arguments[0].click();", next_button)
-
-
-
-def download_pasco(links, choice):
-    """
-    A function to download past questions.
-    It takes in the links of the past questions and the users choice.
-    Returns the path of the downloaded past question.
-    """
-
-    for i, v in links.items():
-        if int(choice) == i:
-            driver.get(v)  # Move to the url of users choice.
-
-    file = driver.find_element(By.CLASS_NAME, "openPopUp")
-    driver.execute_script(
-        "arguments[0].click();", file
-    )  # screen displayed is a frame, so adapts to a frame.
-    wait = WebDriverWait(driver, 10)
-    wait.until(
-        EC.frame_to_be_available_and_switch_to_it(
-            (By.CLASS_NAME, "cboxIframe")
-        )
-    )
-    wait.until(EC.element_to_be_clickable((By.ID, "download"))).click()
-    driver.back()
-    time.sleep(2)  # wait for file to be downloaded before moving on.
-    file = newest(PATH)
-    return file
+# next_button = driver.find_element(By.ID, "pagenumnext")
+# # It's a js button, so need to use execute script
+# driver.execute_script("arguments[0].click();", next_button)
 
 
--- crawler.py
+++ crawler.py
@@ -1,21 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://meqasa.com/houses-for-sale-in-Accra.html?y=965793785&w=1"
 
 def get_details(url):
     # Get url
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "lxml")
 
+
     # Locate where it's stored
     results = soup.find(id="pg1")
+    print(results)
     mqs_class = results.find_all(
-    "div", class_="row mqs-featured-prop-inner-wrap clickable "
+        "div", class_="row mqs-featured-prop-inner-wrap clickable "
     )
-
+    print("What is my name")
+    print(mqs_class)
     data_file = open("data/temp.txt", "w")
-
     for i in mqs_class:
         description = i.find(["h2"]).get_text().strip()  # get description
         price = i.find(["p"], ["h3"]).get_text().strip()  # get the prices
@@ -27,6 +28,12 @@ def get_details(url):
             f"{description}, {price}, {room_num}"
         )  # store it in the text file
         data_file.write("\n")
+        print(f"{description}, {price}, {room_num}")
+
+    data_file.close()
 
 if __name__ == "__main__":
-    get_details(url)
+    print("What is my name")
+    get_details(
+        "https://meqasa.com/houses-for-sale-in-Accra.html?y=965793785&w=1"
+    )
